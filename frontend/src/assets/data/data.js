b6bddd78efa0e21d160f23c17f15744d81b643bd
Denny Ribeiro <denny.ribeiro@outlook.com>
2022-04-17 13:44:57 -0300

    adicionada página about-us; modificações para incluí-la em footer e App

---
+++ frontend/src/assets/data/data.js
@@ -0,0 +1,28 @@
+
+export const data = [
+    {
+      id: 1,
+      nome: "Denny Ribeiro",
+      linkedin: "",
+      github: "",
+    },
+    {
+      id: 2,
+      nome: "João",
+      linkedin: "",
+      github: "",
+    },
+    {
+      id: 3,
+      nome: "Poliana",
+      linkedin: "",
+      github: "",
+    },
+    {
+      id: 4,
+      nome: "Rafael",
+      linkedin: "",
+      github: "",
+    },
+  ];
+  
